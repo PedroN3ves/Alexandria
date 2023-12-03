a1d5c33f238936bf1d9aa83a3d7b0a20396f2bf0
Pedro Neves <111977651+PedroN3ves@users.noreply.github.com>
2023-12-03 12:15:58 -0300

    Security Update

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('alexandria_db', 'root', '@Pe0807#Ma071012', {
+const sequelize = new Sequelize('', '', '', {
     host: '127.0.0.1',
     port: 3306,
     dialect: 'mysql'
